9f14f856d9c39b7764fa3fce4672215f3ceb84c8
asal <asalrazavi2002@gmail.com>
2023-07-30 09:21:56 +0200

    add

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let butten1=document.querySelector(".header-icon");
 let menu=document.querySelector(".menu");
-
+let X=document.querySelector('.X')
 butten1.addEventListener('click',function(){
-    menu.style.left='0'}
+    menu.style.left='0';
+    X.style.display='block'
+    
+  }
   )
+  X.addEventListener('click',function(){
+    menu.style.left='-100%';
+    X.style.display='none'
+  })
